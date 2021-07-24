2123712f39538152e8c09437ac5feb30d9afd3c8
mitchell.wright <mitchell.wright@net-cents.com>
2021-07-23 22:02:12 -0700

    move subscribe fn into constructor, update orderbook with incoming orders or delete them if they have an empty size

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,22 @@
 export const TOGGLE_FEED = "TOGGLE_FEED";
 export const KILL_FEED = "KILL_FEED";
+
+export interface OrderStream {
+  asks: Array<number[]>;
+  bids: Array<number[]>;
+  feed: string;
+  numLevels?: number;
+  product_id: string;
+}
+
+export interface Order {
+  [key: number]: { size: number | string };
+}
+
+export interface OrderBook {
+  asks: Order;
+  bids: Order;
+  numLevels?: number;
+  feed: string;
+  product_id: string;
+}
--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,5 @@
 import { expose, wrap } from "comlink";
-import { KILL_FEED, TOGGLE_FEED } from "./types";
+import { KILL_FEED, Order, OrderBook, OrderStream, TOGGLE_FEED } from "./types";
 
 function testfn() {
   console.log("hi");
@@ -17,41 +17,103 @@ test2();
 class WebSocketStream {
   private ws: WebSocket;
   private ticker: string | undefined;
+  private orderBook: OrderBook;
+  private subscribe: any;
 
   constructor(ticker = "PI_XBTUSD") {
+    this.orderBook = {
+      bids: [],
+      asks: [],
+      numLevels: undefined,
+      product_id: "",
+      feed: "",
+    };
     this.ticker = ticker;
-    // const subscribe = {
-    //   event: "subscribe",
-    //   feed: "book_ui_1",
-    //   product_ids: ["PI_XBTUSD"],
-    // };
+
     this.ws = new WebSocket("wss://www.cryptofacilities.com/ws/v1");
 
-    // this.ws.onopen = () => {
-    // this.subscribe(ticker);
-    // this.ws.send(JSON.stringify(subscribe));
-    // };
-    const subscription = {
-      event: "subscribe",
-      feed: "book_ui_1",
-      product_ids: [ticker],
-    };
+    this.subscribe = () => {
+      const subscription = {
+        event: "subscribe",
+        feed: "book_ui_1",
+        product_ids: [this.ticker],
+      };
+      this.ws = new WebSocket("wss://www.cryptofacilities.com/ws/v1");
+      this.ws.OPEN;
+      this.ws.onopen = () => {
+        console.log("open");
+        this.ws.send(JSON.stringify(subscription));
+      };
+      this.ws.onmessage = (event) => {
+        const parsedData: OrderStream = JSON.parse(event.data);
+        console.log("Stream:", parsedData);
+        switch (parsedData.feed) {
+          case "book_ui_1_snapshot":
+            this.initializeOrderBook(parsedData);
+            break;
+          case "book_ui_1":
+            this.uppdateOrderBook(parsedData);
+            break;
 
-    this.ws.onopen = () => {
-      console.log("open");
-      this.ws.send(JSON.stringify(subscription));
+          default:
+            break;
+        }
+        console.log("this.orderBook", this.orderBook);
+      };
+      this.ws.onclose = () => {
+        this.ws.close();
+      };
     };
+    this.subscribe();
+  }
 
-    this.ws.onmessage = (event) => {
-      const parsedData = JSON.parse(event.data);
-      console.log(parsedData);
-    };
-    this.ws.onclose = () => {
-      this.ws.close();
+  private initializeOrderBook(parsedData: OrderStream) {
+    this.orderBook = {
+      ...parsedData,
+      asks: this.buildOrderBook(parsedData.asks),
+      bids: this.buildOrderBook(parsedData.bids),
     };
   }
 
-  toggleFeed(ticker?: string) {
+  private buildOrderBook(orders: Array<number[]>) {
+    const obj: Order = {};
+    for (const order of orders) {
+      const [price, size] = order;
+      obj[price] = { size };
+    }
+    return obj;
+  }
+
+  private uppdateOrderBook(parsedData: OrderStream) {
+    if (parsedData.asks) {
+      for (const data of parsedData.asks) {
+        const [price, size] = data;
+        if (price in this.orderBook.asks) {
+          if (size === 0) {
+            delete this.orderBook.asks[price];
+          } else {
+            this.orderBook.asks[price] = { size };
+          }
+        }
+      }
+    }
+    if (parsedData.bids) {
+      for (const data of parsedData.bids) {
+        const [price, size] = data;
+        if (price in this.orderBook.bids) {
+          if (size === 0) {
+            delete this.orderBook.bids[price];
+          } else {
+            this.orderBook.bids[price] = { size };
+          }
+        }
+      }
+    }
+  }
+
+  // then group by ticks, totals etc
+
+  public toggleFeed(ticker?: string) {
     console.log(">>>>>>>>>>>>>>>>>", ticker, ">>>>>>>>>>>>>");
     const unsubscribe = {
       event: "unsubscribe",
@@ -68,28 +130,7 @@ class WebSocketStream {
     this.ticker = ticker;
   }
 
-  subscribe() {
-    const subscription = {
-      event: "subscribe",
-      feed: "book_ui_1",
-      product_ids: [this.ticker],
-    };
-    this.ws = new WebSocket("wss://www.cryptofacilities.com/ws/v1");
-    this.ws.OPEN;
-    this.ws.onopen = () => {
-      console.log("open");
-      this.ws.send(JSON.stringify(subscription));
-    };
-    this.ws.onmessage = (event) => {
-      const parsedData = JSON.parse(event.data);
-      console.log(parsedData);
-    };
-    this.ws.onclose = () => {
-      this.ws.close();
-    };
-  }
-
-  unsubscribe() {
+  private unsubscribe() {
     const subscription = {
       event: "unsubscribe",
       feed: "book_ui_1",
@@ -99,13 +140,14 @@ class WebSocketStream {
     this.ws.send(JSON.stringify(subscription));
   }
 
-  killFeed(killStream?: boolean) {
+  public killFeed(killStream?: boolean) {
     console.log(">>>>>>>Test");
     try {
       if (killStream) {
         console.log("+========= yeoooooo =========");
         this.unsubscribe();
         this.ws.close();
+        // clear orderbook state TODO: BUG
         throw "Killed Feed";
       }
       this.subscribe();
